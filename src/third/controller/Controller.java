c1669c30d0b5f923b3d6c31c7973e06b2ad01d04
Ethan <tenseharmony202@gmail.com>
2019-10-02 13:35:32 -0600

    did for/while/do while loops

--- src/third/controller/Controller.java
+++ src/third/controller/Controller.java
@@ -8,8 +8,59 @@ public class Controller
 	public void start()
 	{
 		
-		g = " nope";
-		x = 5;
-		System.out.println(x + g + " hello");
+		workWithLoops();
 	}
+	
+	
+	private void workWithLoops()
+	{
+		//step one: define and initialize variable.
+		boolean isFinished = false;
+		int counter = 0;
+		
+		//step 2: test var
+		while (!isFinished)
+		{
+			//step 3: do stuff
+			System.out.println("this is a loop  " + counter);
+			counter += 1;
+			
+			if (counter > 3)
+			{
+				//step 4: update the var
+				isFinished = true;
+			}	
+		}
+		
+		System.out.println("counter: " + counter);
+		
+		while (counter > 0)
+		{
+			System.out.println(counter);
+			counter--;
+		}
+		
+		for (int position = 0; position < 100; position += 1)
+		{
+			System.out.println("position: " + position);
+		}
+		
+		for (int spot = 100; spot >= 0; spot -= 1)
+		{
+				System.out.println("spot: " + spot);
+		}
+		
+		int special = 1;
+		do
+		{
+			System.out.println("special" + special);
+			special--;
+		}while (special > 0);
+	
+	
+	private void sampleIfBlocks()
+	{
+		
+	}
+	
 }
